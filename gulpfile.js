8e4531fd1a2811e57b4df6455b5f6fe8ffe76317
philippe <maxstraeten@yahoo.com>
2025-04-06 23:11:31 +0200

    création du dossier

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const mjml = require('gulp-mjml');
+const nunjucksRender = require('gulp-nunjucks-render');
+const data = require('gulp-data');
+const fs = require('fs');
+const path = require('path');
+
+//Tâche pour rendre le template Nunjucks avec les données JSON
+gulp.task('nunjucks', function() {
+    return gulp.src('src/mjml/layout.mjml.njk')
+        .pipe(data(function(){
+            return{
+                articles: JSON.parse(fs.readFileSync('src/json/articles.json', 'utf8')),
+                logos: JSON.parse(fs.readFileSync('src/json/logos.json', 'utf8')),
+                breves: JSON.parse(fs.readFileSync('src/json/breves.json', 'utf8')),
+            };
+        }))
+})

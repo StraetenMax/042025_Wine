bc07b036218cf59688a5163dfd67bc20424dd530
philippe <maxstraeten@yahoo.com>
2025-04-07 07:10:10 +0200

    changment de gulp-mjml par mjml, gulp-rename et through2

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
+//const mjml = require('gulp-mjml');
 const gulp = require('gulp');
-const mjml = require('gulp-mjml');
+const mjml = require('mjml');
+const rename = require('gulp-rename');
+const through2 = require('through2');
 const nunjucksRender = require('gulp-nunjucks-render');
 const data = require('gulp-data');
 const fs = require('fs');
 const path = require('path');
+
+//Erreur mjml
+function handleError (err) {
+    console.log(err.toString());
+    this.emit('end');
+  }
 
 //Tâche pour rendre le template Nunjucks avec les données JSON
 gulp.task('nunjucks', function() {
@@ -24,12 +33,19 @@ gulp.task('nunjucks', function() {
 //Tâche pour compiler MJML en HTML
 gulp.task('mjml', function(){
     console.log('Starting mjml task...');
-    return gulp.src('dist/layout.mjml',{ allowEmpty: true } )
-        .pipe(mjml({
+    return gulp.src('dist/layout.mjml', { allowEmpty: true } )
+        /*.pipe(mjml(mjmlEngine, {
             beautify: false,
             validationLevel: 'strict',
             keepComments: false
         }))
+        .on('error', handleError)*/
+        .pipe(through2.obj(function(file, _, cb) {
+            const { html } = mjml(file.contents.toString());
+            file.contents = Buffer.from(html);
+            cb(null, file);
+          }))
+        .pipe(rename({ extname: '.html' }))
         .pipe(gulp.dest('dist'))
         .on('end', () => console.log('MJML task completed.'));
 });
